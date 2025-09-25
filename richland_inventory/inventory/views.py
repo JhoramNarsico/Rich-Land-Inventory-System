37628e1b7b02aacb43a04f87ad38c4ebfff770d4
Joseph Alberto <your-github-email@example.com>
2025-09-25 20:01:47 +0800

    - Added a complete login/logout system for users. - Protected all inventory pages so they require a user to be logged in. - Created a separate login page for regular users at /accounts/login/. - Added a link on the user login page to navigate to the admin login. - Overrode the admin login template to add a link back to the user login page. - Configured settings to redirect users correctly after login and logout.

--- richland_inventory/inventory/views.py
+++ richland_inventory/inventory/views.py
@@ -1,32 +1,34 @@
 # inventory/views.py
 
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Product
 
-class ProductListView(ListView):
+class ProductListView(LoginRequiredMixin, ListView):
     model = Product
     context_object_name = 'product_list' # Custom name for the list in the template
     template_name = 'inventory/product_list.html'
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = 'inventory/product_detail.html'
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     fields = ['name', 'sku', 'price', 'quantity'] # Fields to display in the form
     template_name = 'inventory/product_form.html'
     success_url = reverse_lazy('product-list') # Redirect to the list view after creation
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     fields = ['name', 'sku', 'price', 'quantity']
     template_name = 'inventory/product_form.html'
     success_url = reverse_lazy('product-list') # Redirect after update
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'inventory/product_confirm_delete.html'
     success_url = reverse_lazy('product-list')
