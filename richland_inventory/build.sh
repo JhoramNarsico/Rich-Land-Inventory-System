2c9f22bbfdfcec36877a444f4392a9b5e9e55a38
JhoramNarsico <jhoramnarsico@gmail.com>
2025-11-17 19:22:00 +0800

    temp fix

--- richland_inventory/build.sh
+++ richland_inventory/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# --- ADD THIS LINE TEMPORARILY ---
+# The --noinput flag tells it to read credentials from environment variables.
+python manage.py createsuperuser --noinput
