1331a42fb3ceed04f6077bdaf7a48e3e2850b6d0
JhoramNarsico <20220024450@my.xu.edu.ph>
2025-11-20 12:58:46 +0800

    remove pymsql

--- richland_inventory/core/__init__.py
+++
@@ -1,9 +0,0 @@
-# core/__init__.py
-
-import pymysql
-
-# This line tells Django to use PyMySQL
-pymysql.install_as_MySQLdb()
-
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
