2d2739d3531c8ccdc08bace2e1b12ec3c0f59fd6
JhoramNarsico <20220024450@my.xu.edu.ph>
2025-11-21 21:54:30 +0800

    added: sign out automatically feature

--- richland_inventory/core/urls.py
+++ richland_inventory/core/urls.py
@@ -4,27 +4,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# --- Tier 2: Add drf-spectacular imports ---
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from inventory.views import CustomLoginView # <--- IMPORT THIS
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    
+    # --- USE CUSTOM LOGIN VIEW ---
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    
+    # Keep the rest for logout/password reset
     path('accounts/', include('django.contrib.auth.urls')),
     
-    # Your existing API URLs
     path('api/', include(('inventory.api_urls', 'inventory-api'))),
-    
-    # --- Tier 2: Add these URLs for the API documentation ---
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Swagger UI:
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    # Redoc UI:
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
-    # Your main inventory app URLs
     path('inventory/', include('inventory.urls')),
 ]
 
